57f4cad2da5a7e6e867dcc2dd6af60360dddd5eb
William Mariadason <william.mariadason@gmail.com>
2021-05-13 10:23:41 +0200

    added trailing '/' to g-links

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -14,10 +14,10 @@
     </header>
     <nav>
       <ul>
-        <li><g-link to="/om-oss">Om oss</g-link></li>
-        <li><g-link to="/komiteer">Komiteer</g-link></li>
-        <li><g-link to="/kompendier">kompendier</g-link></li>
-        <li><g-link to="/new-student">Ny student</g-link></li>
+        <li><g-link to="/om-oss/">Om oss</g-link></li>
+        <li><g-link to="/komiteer/">Komiteer</g-link></li>
+        <li><g-link to="/kompendier/">kompendier</g-link></li>
+        <li><g-link to="/new-student/">Ny student</g-link></li>
       </ul>
     </nav>
     <div class="body">
