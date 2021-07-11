eaf5b0a79390c0b18810eb610b6dd58b4ab12e6e
William Mariadason <william.mariadason@gmail.com>
2021-07-11 16:42:44 +0200

    made all sizes in index relative (px --> rem)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,57 @@
 <template>
   <Layout>
     <header id="second-header">
-      <g-image src="~/assets/img/gruppebilde-kull21.jpg" class = "header-image"/>
+      <g-image src="~/assets/img/gruppebilde-kull21.jpg" class="header-image" />
       <div class="body">
-        <p>Skal du studere årsstudium/bachelor/master i matematiske fag eller bachelor/master i fysikk ved NTNU til høsten? Meld deg på i Deltas fadderuke!</p>
-        <a href="https://forms.gle/G8RM8x3SV9NbEiySA" class="link-button">Søk her</a>
+        <p>
+          Skal du studere årsstudium/bachelor/master i matematiske fag eller
+          bachelor/master i fysikk ved NTNU til høsten? Meld deg på i Deltas
+          fadderuke!
+        </p>
+        <a href="https://forms.gle/G8RM8x3SV9NbEiySA" class="link-button"
+          >Søk her</a
+        >
       </div>
     </header>
-      <section id="kompendier">
-          <h1>Kompendier</h1>
-          <ul>
-            <li><g-link to="/kompendier"><g-image width="200" src="~/assets/img/MA1101.jpg" alt="MA1101"/></g-link></li>
-            <li><g-link to="/kompendier"><g-image width="200" src="~/assets/img/MA1102.jpg" alt="MA1102"/></g-link></li>
-            <li><g-link to="/kompendier"><g-image width="200" src="~/assets/img/MA1201.jpg" alt="MA1201"/></g-link></li>
-            <li><g-link to="/kompendier"><g-image width="200" src="~/assets/img/TFY4102.jpg" alt="TFY102"/></g-link></li>
-            <li><g-link to="/kompendier"><g-image width="200" src="~/assets/img/TFY4109.jpg" alt="MA1102"/></g-link></li>
-          </ul>
-        <g-link class=link-button to="/kompendier">Se alle</g-link>
-      </section>
-      <section id="nyheter">
-        <h2>Nyheter</h2>
-        <news-card v-for="edge in $page.news.edges" :key="edge.node.id" :post="edge.node"/>  
-      </section>
+    <section id="kompendier">
+      <h1>Kompendier</h1>
+      <ul>
+        <li>
+          <g-link to="/kompendier"
+            ><g-image width="200" src="~/assets/img/MA1101.jpg" alt="MA1101"
+          /></g-link>
+        </li>
+        <li>
+          <g-link to="/kompendier"
+            ><g-image width="200" src="~/assets/img/MA1102.jpg" alt="MA1102"
+          /></g-link>
+        </li>
+        <li>
+          <g-link to="/kompendier"
+            ><g-image width="200" src="~/assets/img/MA1201.jpg" alt="MA1201"
+          /></g-link>
+        </li>
+        <li>
+          <g-link to="/kompendier"
+            ><g-image width="200" src="~/assets/img/TFY4102.jpg" alt="TFY102"
+          /></g-link>
+        </li>
+        <li>
+          <g-link to="/kompendier"
+            ><g-image width="200" src="~/assets/img/TFY4109.jpg" alt="MA1102"
+          /></g-link>
+        </li>
+      </ul>
+      <g-link class="link-button" to="/kompendier">Se alle</g-link>
+    </section>
+    <section id="nyheter">
+      <h2>Nyheter</h2>
+      <news-card
+        v-for="edge in $page.news.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
+    </section>
   </Layout>
 </template>
 
@@ -41,34 +71,34 @@
 </page-query>
 
 <script>
-import NewsCard from '../components/NewsCard.vue'
+import NewsCard from "../components/NewsCard.vue";
 export default {
   components: { NewsCard },
   metaInfo: {
-    title: 'Delta - linjeforeningen for matte og fysikk ved NTNU'
-  }
-}
+    title: "Delta - linjeforeningen for matte og fysikk ved NTNU",
+  },
+};
 </script>
 
 <style>
-  *{
-    padding: 0;
-    border: 0;
-    margin: 0;
-  }
-  .header-image{
-    width: 100vw
-  }
-  :root{
-    --deltaGreen: #005500;
-    --buttonGreen: #002500;
-  }
-  html {
-    width: 100vw;
-  }
-  #second-header {
+* {
+  padding: 0;
+  border: 0;
+  margin: 0;
+}
+.header-image {
+  width: 100vw;
+}
+:root {
+  --deltaGreen: #005500;
+  --buttonGreen: #002500;
+}
+html {
+  width: 100vw;
+}
+#second-header {
   background-color: var(--deltaGreen);
-  font-size: 40px;
+  font-size: 4rem;
   text-align: center;
   color: white;
   font-weight: 700;
@@ -85,7 +115,7 @@ export default {
   margin: 0 0 3rem 0;
 }
 #second-header .body {
-  padding: 100px 240px;
+  padding: 10rem 24rem;
 }
 
 a {
@@ -128,9 +158,8 @@ a {
 }
 #nyheter h2 {
   color: white;
-  margin: 0 0 40px 0;
+  margin: 0 0 4rem 0;
   text-align: center;
   font-size: 3rem;
 }
-
 </style>
