39e519d6e38acb06c3c057a996a65175d9de0409
Antony William Mariadason <william.mariadason@gmail.com>
2023-01-19 17:58:07 +0100

    main

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -49,7 +49,7 @@
 
 <page-query>
   query{
-    news: allPosts(filter: {layout: {eq: "news"}}sortBy:"date" order:ASC perPage: 4){
+    news: allPosts(filter: {layout: {eq: "news"}}sortBy:"date" order:DESC perPage: 4){
       edges{
         node{
           path
@@ -83,7 +83,7 @@ export default {
   width: 100vw;
 }
 :root {
-  --deltaGreen: #005500;
+  --deltaGreen: #115027;
   --buttonGreen: #002500;
 }
 html {
